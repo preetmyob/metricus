4ef81c3cab2d5b2158f01da2137c8e3bcd01e5c5
kamal-myob <kamal.deep@myob.com>
2025-11-07 13:15:03 +1300

    updated metricus version to 1.2.0

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Reflection;
 // Version information for all assemblies
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
